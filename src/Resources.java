e2dc6bfab7c563aaf8223213e76585ba423c236f
kandu009 <kandu009@umn.edu>
2015-04-19 12:55:29 -0500

    adding some bookmarks for resuming preparation and some good resources

---
+++ src/Resources.java
@@ -0,0 +1,6 @@
+
+public class Resources {
+	/**
+	 * bigocheatsheet.com
+	 */
+}
---
+++ src/todo/ResumePreparation.java
@@ -0,0 +1,12 @@
+package todo;
+
+public class ResumePreparation {
+
+	/**
+	 * www.careercup.com/question?id=5434428369141760
+	 * www.careercup.com/question?id=15992667
+	 * www.careercup.com/question?id=5147801809846272
+	 * www.careercup.com/page?pid=software-engineer-intern-interview-questions
+	 * www.careercup.com/question?id=5646285971390464
+	 */
+}
