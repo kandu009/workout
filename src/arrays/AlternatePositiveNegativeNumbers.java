6d1a606906d95d52160bd5f25d6a3d8cdd83877c
kandu009 <kandu009@umn.edu>
2018-08-08 09:55:09 -0700

    Update AlternatePositiveNegativeNumbers.java
    fixing

--- src/arrays/AlternatePositiveNegativeNumbers.java
+++ src/arrays/AlternatePositiveNegativeNumbers.java
@@ -16,81 +16,77 @@ package arrays;
  */
 public class AlternatePositiveNegativeNumbers {
 
-	private static void modifyArray(int[] A) {
+	 public static Integer nextPositive(Integer[] input, Integer index) {
+        for (int i = index; i < input.length; ++i) {
+            if (input[i] > 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-		if(A == null || A.length == 1) {
-			return;
-		}
-		
-		boolean fillPositive = (A[0] < 0);
-		
-		int fp = findNextPositiveIndex(A, 0); 
-		int fn = findNextNegativeIndex(A, 0);
-		
-		if(fillPositive) {
-			fn = findNextNegativeIndex(A, 1);
-		} else {
-			fp = findNextPositiveIndex(A, 1);
-		}
-		
-		int index = 1;
-		for(;index < A.length && fp > 0 && fn > 0 && fp < A.length && fn < A.length;) {
-			
-			if(fillPositive) {
-				rotate(A, index, fp);
-				fp = findNextPositiveIndex(A, index+1);
-				fn = findNextNegativeIndex(A, index+1);
-			} else {
-				rotate(A, index, fn);
-				fn = findNextNegativeIndex(A, index+1);
-				fp = findNextPositiveIndex(A, index+1);
-			}
-			++index;
-			fillPositive = !fillPositive;
+    public static Integer nextNegative(Integer[] input, Integer index) {
+        for (int i = index; i < input.length; ++i) {
+            if (input[i] < 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-		}
-		
-	}
-	
-	private static int findNextNegativeIndex(int[] A, int start) {
-		for(int i = start; i < A.length; ++i) {
-			if(A[i] < 0) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	private static int findNextPositiveIndex(int[] A, int start) {
-		for(int i = start; i < A.length; ++i) {
-			if(A[i] > 0) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	private static void rotate(int[] A, int start, int end) {
-		if(start >= end) {
-			return;
-		}
-		int temp = A[end];
-		for(int i = end; i > start; --i) {
-			A[i] = A[i-1];
-		}
-		A[start] = temp;
-	}
+    public static void rotateElements(Integer[] input, Integer index1, Integer index2) {
+        if (index1 >= index2) {
+            return;
+        }
+        Integer temp = input[index2];
+        for (int i = index2; i > index1; --i) {
+            input[i] = input[i - 1];
+        }
+        input[index1] = temp;
+    }
 
-	public static void main(String[] args) {
+    public static void modifyArray(Integer[] input) {
 
-		int[] A = new int[] {-6, -5, -4, -3, 2, 1, 1, 2 };
-		
-		modifyArray(A);
-		
-		for(int i = 0; i < A.length; ++i) {
-			System.out.println(A[i]);
-		}
-		
-	}
+        if (input == null || input.length <= 1) {
+            return;
+        }
+
+        boolean isPositive = true;
+
+        Integer pi = nextPositive(input, 0);
+        Integer ni = nextNegative(input, 0);
+        Integer index = 0;
+
+        for (; pi >= 0 && pi < input.length && ni >= 0 && ni < input.length && index < input.length; ) {
+            if (isPositive) {
+                rotateElements(input, index, pi);
+                ni = nextNegative(input, index + 1);
+                pi = nextPositive(input, index + 1);
+            } else {
+                rotateElements(input, index, ni);
+                ni = nextNegative(input, index + 1);
+                pi = nextPositive(input, index + 1);
+            }
+            index++;
+            isPositive = !isPositive;
+        }
+
+        StringJoiner joiner = new StringJoiner(" ");
+        for (int i = 0; i < input.length; i++) {
+            joiner.add(String.valueOf(input[i]));
+        }
+        System.out.println(joiner.toString());
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numOfInputs = in.nextInt();
+        Integer[] input = new Integer[numOfInputs];
+        for (int i = 0; i < numOfInputs; i++) {
+            input[i] = in.nextInt();
+        }
+        modifyArray(input);
+    }
 
 }
